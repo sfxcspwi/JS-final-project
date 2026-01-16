362e6a0d69fbeaaa5b3970dbbf307bf3d2341eb3
sfxcspwi <staschuguivec@gmail.com>
2026-01-16 17:40:29 +0200

    header

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const closeBtn = document.querySelector(".modal__close");
 const modalSaveBtn = document.querySelector(".modal__save");
 const inputName = document.querySelector(".modal__input")
 const userNameOut = document.querySelector("#userName");
+const interactiveLink = document.querySelector("#header__interactive-Link")
+const submenu = document.querySelector("#interactiveSubmenu");
+
 
 function closeModal() 
 {
@@ -24,6 +27,34 @@ document.addEventListener("keydown", event => {
 modalSaveBtn.addEventListener("click", event => {
     const name = inputName.value
     if (name === "") return; 
+    if (name.length > 16)
+    {
+        alert("Максимальна кількість символів 16!")
+        return
+    }
+    
     userNameOut.textContent = `Вітаємо, ${name}!`;
     closeModal();
+});
+
+submenu.style.display = 'none';
+
+interactiveLink.addEventListener('click', event => {
+    event.preventDefault(); 
+
+    if (submenu.style.display === 'none') {
+        submenu.style.display = 'flex'; 
+    } 
+    else 
+    {
+        submenu.style.display = 'none'; 
+    }
+});
+
+
+document.addEventListener('click', event => {
+    if (!interactiveLink.contains(event.target) && !submenu.contains(event.target)) 
+    {
+        submenu.style.display = 'none';
+    }
 });
