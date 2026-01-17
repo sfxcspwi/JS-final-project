212ed1f8132af52df9cd3c16fad3dde2223b562d
sfxcspwi <staschuguivec@gmail.com>
2026-01-17 14:23:12 +0200

    upd

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const modal = document.querySelector("#modal");
-const backdrop = document.querySelector(".modal__backdrop");
-const closeBtn = document.querySelector(".modal__close");
-const modalSaveBtn = document.querySelector(".modal__save");
-const inputName = document.querySelector(".modal__input")
-const userNameOut = document.querySelector("#userName");
-const interactiveLink = document.querySelector("#header__interactive-Link")
-const submenu = document.querySelector("#interactiveSubmenu");
+let modal = document.querySelector("#modal");
+let backdrop = document.querySelector(".modal__backdrop");
+let closeBtn = document.querySelector(".modal__close");
+let modalSaveBtn = document.querySelector(".modal__save");
+let inputName = document.querySelector(".modal__input")
+let userNameOut = document.querySelector("#userName");
+let interactiveLink = document.querySelector("#header__interactive-Link")
+let submenu = document.querySelector("#interactiveSubmenu");
+let ageInput = document.querySelector(".age__check-input")
+let ageOut = document.querySelector(".age__check-answer")
+let ageCheckBtn = document.querySelector(".age__check-btn")
+let numberInput = document.querySelector(".guess-number__input")
+let numberOut = document.querySelector(".guess-number__answer")
+let numberCheckBtn = document.querySelector(".guess-number__btn")
+
+numberCheckBtn.addEventListener("click", event => {
+    let computerNumber = Math.floor(Math.random() * 10) + 1;
+    if( computerNumber === Number(numberInput.value))
+    {
+        numberOut.textContent = `Вітаю, ви вгадали число! ${computerNumber}`
+        numberOut.style.color = "#039900"
+    }
+    else
+    {
+        numberOut.textContent = `Ви програли, комп’ютер загадав ${computerNumber}`
+        numberOut.style.color = "#990000"
+    }
+})
+
+ageCheckBtn.addEventListener("click", event => {
+    if(Number(ageInput.value) % 4 === 0)
+    {
+        ageOut.textContent = "Ви народилися у високосний рік!"
+        ageOut.style.color = "#039900"
+    }
+    else
+    {
+        ageOut.textContent = "Ви народилися не у високосний рік!"
+        ageOut.style.color = "#990000"
+    }
+})
+
 
 
 function closeModal() 
