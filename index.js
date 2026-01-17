ca020c75a2a7c74c5a1e036c527c8eab335e3960
sfxcspwi <staschuguivec@gmail.com>
2026-01-17 18:14:23 +0200

    rps

--- index.js
+++ index.js
@@ -12,6 +12,62 @@ let ageCheckBtn = document.querySelector(".age__check-btn")
 let numberInput = document.querySelector(".guess-number__input")
 let numberOut = document.querySelector(".guess-number__answer")
 let numberCheckBtn = document.querySelector(".guess-number__btn")
+let rpsBtns = document.querySelectorAll(".rps__btn-user");
+let rpsResult = document.querySelector(".rps__result");
+let rpsBtnCheck = document.querySelector(".rps__btn-check");
+let computerScoreEl = document.querySelector(".computer__stats");
+let userScoreEl = document.querySelector(".user__stats");
+
+const choices = ["rock", "scissors", "paper"];
+let computerScore = 0;
+let userScore = 0;
+let playerChoice = ""; 
+
+rpsBtns.forEach(button => 
+{
+    button.addEventListener("click", () => {
+        playerChoice = button.dataset.choice;
+    });
+});
+
+
+rpsBtnCheck.addEventListener("click", () => {
+    if (playerChoice === "") 
+    {
+        return;
+    }
+
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let winner = "";
+
+    if (playerChoice === computerChoice) 
+    {
+        winner = "Нічия";
+        rpsResult.style.color = "black"
+    } 
+    else if 
+    (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'rock')
+    ) 
+    {
+        winner = "Ви виграли раунд!";
+        userScore++;
+        rpsResult.style.color = "#039900"
+    } 
+    else 
+    {
+        winner = "Комп’ютер виграв раунд!";
+        rpsResult.style.color = "#990000"
+        computerScore++;
+    }
+
+    rpsResult.textContent = winner;
+    computerScoreEl.textContent = `Комп'ютер - ${computerScore}`;
+    userScoreEl.textContent = `Ви - ${userScore}`;
+});
+
 
 numberCheckBtn.addEventListener("click", event => {
     let computerNumber = Math.floor(Math.random() * 10) + 1;
